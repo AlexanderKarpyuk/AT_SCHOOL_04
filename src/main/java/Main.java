17be48cd510e2e130fbc2e4a2fb1ed97211594f7
Alexander <karjuk.sasha@gmail.com>
2020-07-02 18:59:52 +0400

    Добавил чуточку рефлексии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,36 @@
+
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import somepackage.Other;
 import somepackage.Some;
-import somethinggreen.Contract;
+import somethinggreen.AnnotationForExaple;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
+        Other.saySomthing();
+        Some some = new Some("Секрет");
+        System.out.println(some.getДаТакТожеМожно());
+        System.out.println(some);
+        some.setNoMoreSecrets(some.getДаТакТожеМожно());
+        System.out.println(some.getSecret(true));
+        System.out.println(some);
+        some.soTellMeAboutInterfaces();
+        System.out.println();
+
+        AnnotationForExaple annotation = Some.class.getAnnotation(AnnotationForExaple.class);
+        System.out.println(annotation.description());
+
+        try {
+            String defaultValue = (String) AnnotationForExaple.class
+                    .getDeclaredMethod("description").getDefaultValue();
+            System.out.println(defaultValue);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
     }
 }
