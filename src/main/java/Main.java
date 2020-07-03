8f40849b95ec58638433a950c1e8df2da71db9dd
Alexander <karjuk.sasha@gmail.com>
2020-07-03 09:47:40 +0400

    Вывод в консоль скрытых строк

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,13 @@
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import somepackage.Other;
 import somepackage.Some;
 import somethinggreen.AnnotationForExaple;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
         Other.saySomthing();
         Some some = new Some("Секрет");
-        System.out.println(some.getДаТакТожеМожно());
-        System.out.println(some);
+        System.out.println(some.getSecret());
         some.setNoMoreSecrets(some.getДаТакТожеМожно());
         System.out.println(some.getSecret(true));
         System.out.println(some);
